dcfb404c25019b8ff0684167473a5b0aeab66360
Raimund Andree <r.andree@live.com>
2023-09-05 18:38:46 +0200

    Quick-fixed an issue connecting to Exchange online.

--- .build/Fix.ps1
+++ .build/Fix.ps1
@@ -4,11 +4,34 @@ task FixMSCloudLoginAssistant {
     $p = Resolve-Path -Path "$RequiredModulesDirectory\MSCloudLoginAssistant\*\Workloads\MicrosoftGraph.psm1"
     $c = Get-Content -Path $p
 
-    if ($c -like "*$missingLine*") {
-        return
+    if ($c -notlike "*$missingLine*")
+    {
+        $c[84] = $missingLine
+        $c | Set-Content -Path $p
     }
 
-    $c[84] = $missingLine
-    $c | Set-Content -Path $p
     
+    #--------------------------------------------------------------------
+
+    $oldLine = '                -Organization $Global:MSCloudLoginConnectionProfile.OrganizationName `'
+    $newLine = '                -Organization $Global:MSCloudLoginConnectionProfile.ExchangeOnline.TenantId `'
+
+    $p = Resolve-Path -Path "$RequiredModulesDirectory\MSCloudLoginAssistant\*\Workloads\ExchangeOnline.psm1"
+    $c = Get-Content -Path $p
+
+    $lineNumbers = foreach ($line in $c)
+    {
+        if ($line -eq $oldLine)
+        {
+            $line.ReadCount - 1
+        }
+    }
+
+    foreach ($lineNumber in $lineNumbers)
+    {
+        $c[$lineNumber] = $newLine
+    }
+    
+    $c -replace $oldLine, $newLine | Set-Content -Path $p
+
 }
