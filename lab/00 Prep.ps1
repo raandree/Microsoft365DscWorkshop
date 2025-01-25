43193f2e1b7b9d2f50aa0cc79ac66cb9339d856f
Raimund Andree <r.andree@live.com>
2025-01-25 12:14:40 +0100

    Add checks for Git and Azure DevOps repository in prep script

--- lab/00 Prep.ps1
+++ lab/00 Prep.ps1
@@ -1,6 +1,38 @@
+Write-Host 'Checking if the script is running in a git repository...' -NoNewline
+$projectUrl = git remote get-url origin *>&1
+
+if ($projectUrl -like '*fatal: not a git repository*' -and $LASTEXITCODE -eq 128)
+{
+    Write-Host 'Not working in a git repository. Please run the script in a git repository.'
+    return
+}
+else
+{
+    Write-Host 'ok.'
+}
+
+Write-Host 'Checking if the script is running in an Azure DevOps repository...' -NoNewline
+if ($projectUrl -notmatch 'https:\/\/(?<OrgName>[\w-]+@)dev.azure.com')
+{
+    Write-Host 'The script is not running in an Azure DevOps repository. Please run the script in an Azure DevOps repository.'
+    return
+}
+else
+{
+    Write-Host 'ok.'
+}
+
+$projectSettings = Get-Content -Path $PSScriptRoot\..\source\Global\ProjectSettings.yml
+if ($projectSettings -contains 'ProjectName: <ProjectName>')
+{
+    Write-Host "Updating the project name in the 'ProjectSettings.yml' file"
+    $projectSettings = $projectSettings -replace '<ProjectName>', (Split-Path -Path (git rev-parse --show-toplevel) -Leaf)
+    $projectSettings | Out-File -FilePath $PSScriptRoot\..\source\Global\ProjectSettings.yml
+}
+
 $requiredModules = @{
-    VSTeam                                = '7.15.2'
-    AutomatedLab                          = 'latest'
+    VSTeam       = '7.15.2'
+    AutomatedLab = 'latest'
 }
 
 foreach ($module in $requiredModules.GetEnumerator())
